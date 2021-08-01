ee0d5e8ad4c6e124d0544a02ec2ede313cee9786
rajvardhan <rajvardhanmishra24@gmail.com>
2021-08-01 10:45:53 +0530

    commit1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const mongoose = require("mongoose");
 const app = express();
 
